5d66423d29c36f8bae8c9b69d1f760c4644f52e9
Thiago Bardella <thiago.bardella@gmail.com>
2019-03-31 21:18:50 -0300

    Exercise 3 without documentation

---
+++ src/main/java/exercise3/implementations/StreamImpl.java
@@ -0,0 +1,47 @@
+package exercise3.implementations;
+
+import exercise3.interfaces.Stream;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StreamImpl implements Stream {
+
+    private String content;
+    private int currentPosition = -1;
+
+    public StreamImpl(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public char getNext() {
+        if (content.isEmpty()) return Character.MIN_VALUE;
+
+        int lastPosition = content.length() - 1;
+        if (currentPosition != lastPosition) {
+            currentPosition++;
+            return content.toCharArray()[currentPosition];
+        }
+        return Character.MIN_VALUE;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (content.isEmpty()) return false;
+
+        int lastPosition = content.length() - 1;
+        return currentPosition != lastPosition;
+    }
+
+    @Override
+    public char getFirstChar() {
+        if (content.isEmpty()) return Character.MIN_VALUE;
+        return content.charAt(0);
+    }
+
+    @Override
+    public Boolean isDuplicated(char input) {
+        return content.indexOf(String.valueOf(input)) !=
+                content.lastIndexOf(String.valueOf(input));
+    }
+}
---
+++ src/main/java/exercise3/interfaces/Stream.java
@@ -0,0 +1,10 @@
+package exercise3.interfaces;
+
+public interface Stream {
+
+    public char getNext();
+    public boolean hasNext();
+    public char getFirstChar();
+    public Boolean isDuplicated(char input);
+    
+}
